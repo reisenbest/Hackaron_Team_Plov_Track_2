7c7c9ac945a50ff22fb9a314043a8d80acd68e34
reisenbest <drgn96rd@gmail.com>
2023-11-25 17:55:07 +0300

    try to deploy

--- Hackaton_SBER/manage.py
+++ Hackaton_SBER/manage.py
@@ -2,7 +2,18 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import os
+from django.core.management import execute_from_command_line
+from django.contrib.auth.models import User
 
+def create_superuser():
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@admin.com', 'password1234')
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Hackaton_SBER.settings")
+    create_superuser()  # Создание суперпользователя перед выполнением других команд
+    execute_from_command_line()
 
 def main():
     """Run administrative tasks."""
