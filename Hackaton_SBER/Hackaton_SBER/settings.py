76bc4610a765227208cf865d3c655b32d0eda086
reisenbest <drgn96rd@gmail.com>
2023-11-25 12:05:54 +0300

    add document package+crud+small fixes

--- Hackaton_SBER/Hackaton_SBER/settings.py
+++ Hackaton_SBER/Hackaton_SBER/settings.py
@@ -91,25 +91,25 @@ WSGI_APPLICATION = 'Hackaton_SBER.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-
 if DEBUG:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': env.str('DATABASE_NAME'),
-            'USER': env.str('DATABASE_USER'),
-            'PASSWORD': env.str('DATABASE_PASSWORD'),
-            'HOST': env.str('DATABASE_HOST'),
-            'PORT': env.str('DATABASE_PORT'),
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': env.str('DATABASE_NAME'),
+#             'USER': env.str('DATABASE_USER'),
+#             'PASSWORD': env.str('DATABASE_PASSWORD'),
+#             'HOST': env.str('DATABASE_HOST'),
+#             'PORT': env.str('DATABASE_PORT'),
+#         }
+#     }
 else:
     DATABASES = {
         'default': {
